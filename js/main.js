c43dc953481df90eb172b7e13be4a9424d79bb91
HamzaMasood1 <hamzamasood183@gmail.com>
2020-11-16 22:49:14 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+var hotelInfo;
+var details;
+var xhr = new XMLHttpRequest();
+xhr.open('GET', "data.json", true);
+xhr.responseType = 'text';
+xhr.send();
+
+
+xhr.onload = function () {
+    if (xhr.status === 200) {
+        hotelInfo = JSON.parse(xhr.responseText);
+        console.log(hotelInfo);
+
+    } // end if
+} // end function
+
+function display(x) {
+    console.log(x);
+    document.getElementById('roomName').innerHTML = hotelInfo[x].name;
+}
