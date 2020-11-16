33d7a97ad65d96f92b15dccf59e64fdc35b21e89
HamzaMasood1 <hamzamasood183@gmail.com>
2020-11-16 23:04:47 +0000

    finished picture, cost, and amenities

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ xhr.onload = function () {
     if (xhr.status === 200) {
         hotelInfo = JSON.parse(xhr.responseText);
         console.log(hotelInfo);
+        display(0);
 
     } // end if
 } // end function
@@ -20,4 +21,11 @@ function display(x) {
     document.getElementById('roomName').innerHTML = hotelInfo[x].name;
     document.getElementById('desc').innerHTML = hotelInfo[x].description;
     document.getElementById('photo').src = hotelInfo[x].photo;
+    document.getElementById('weekday').innerHTML = hotelInfo[x].cost.weekday;
+    document.getElementById('weekend').innerHTML = hotelInfo[x].cost.weekend;
+    let details = '';
+    for (let i = 0; i < hotelInfo[x].details.length; i++) {
+        details += "<p>" + hotelInfo[x].details[i] + "/p";
+    }
+    document.getElementById('details').innerHTML = details;
 }
